3dd05ffe33856a290fb0c52e7c3e87c27e8babc6
dxwarlock <dxwarlock@gmail.com>
2022-09-28 12:54:27 -0400

    -
    -

--- room.terminal.js
+++ room.terminal.js
@@ -1,6 +1,7 @@
 module.exports = {
     run: function (spawn) {
         var link = spawn.room.find(FIND_STRUCTURES, { filter: (i) => i.structureType == STRUCTURE_TERMINAL });
+        
         for (var i in link) {
             if (spawn.name == 'Spawn1') {
                 Memory.containers.recv = link[i];
